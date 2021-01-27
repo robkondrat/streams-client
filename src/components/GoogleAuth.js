7ed1b616119ed0a9f72ba708b6a1a71a38aaa381
EatYourSoup7 <r.kondratowicz1@gmail.com>
2021-01-27 17:17:45 -0600

    sign in/out works

--- src/components/GoogleAuth.js
+++ src/components/GoogleAuth.js
@@ -11,17 +11,46 @@ class GoogleAuth extends React.Component {
       }).then(() => {
         this.auth = window.gapi.auth2.getAuthInstance();
         this.setState({ isSignedIn: this.auth.isSignedIn.get() })
+        this.auth.isSignedIn.listen(this.onAuthChange);
       });
     });
   }
 
+  onAuthChange = () => {
+    this.setState({ isSignedIn: this.auth.isSignedIn.get() });
+  };
+
+  onSignIn = () => {
+    this.auth.signIn();
+  };
+
+  onSignOut = () => {
+    this.auth.signOut();
+  };
+
   renderAuthButton() {
     if (this.state.isSignedIn === null) {
-      return <div>I don't know if signed in</div>
+      return null;
     } else if (this.state.isSignedIn) {
-      return <div>I'm signed in</div>
+      return (
+        <button 
+          className="ui red google button"
+          onClick={this.onSignOut}
+        >
+          <i className="google icon" />
+          Sign Out
+        </button>
+      )
     } else {
-      return <div>NOT SIGNED IN</div>;
+      return (
+        <button 
+          className="ui red google button"
+          onClick={this.onSignIn}
+        >
+          <i className="google icon" />
+          Sign In with Google
+        </button>
+      )
     }
   }
 
