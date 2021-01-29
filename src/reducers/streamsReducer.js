2b81ea00cfe1c38e5a0fa661bb991044e421ce30
EatYourSoup7 <r.kondratowicz1@gmail.com>
2021-01-29 14:00:52 -0600

    streams reducers for each RESTful action

---
+++ src/reducers/streamsReducer.js
@@ -0,0 +1,13 @@
+import { EDIT_STREAM } from "../actions/types";
+
+const streamReducer = (state={}, action) => {
+  switch (action.type) {
+    case EDIT_STREAM:
+      // const newState = { ...state };
+      // newState[action.payload.id] = action.payload;
+      // return newState;
+      return { ...state, [action.payload.id]: action.payload };
+    default:
+      return state;
+  }
+};
