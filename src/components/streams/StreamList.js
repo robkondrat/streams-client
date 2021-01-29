933a71e818ae83a0efa9d7f1a3ebbe942ba2e829
EatYourSoup7 <r.kondratowicz1@gmail.com>
2021-01-29 14:27:17 -0600

    Create Stream button renders for logged in users only

--- src/components/streams/StreamList.js
+++ src/components/streams/StreamList.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { fetchStreams } from '../../actions';
+import { Link } from 'react-router-dom';
 
 class StreamList extends React.Component {
   componentDidMount() {
@@ -39,6 +40,16 @@ class StreamList extends React.Component {
     })
   }
 
+  renderCreate() {
+    if (this.props.isSignedIn) {
+      return (
+        <div style={{ textAlign: 'right '}}>
+          <Link to="/streams/new" className="ui button primary">Create Stream</Link>
+        </div>
+      )
+    }
+  }
+
   render() {
     return (
       <div>
@@ -46,6 +57,7 @@ class StreamList extends React.Component {
         <div className="ui celled list">
           {this.renderList()}
         </div>
+        {this.renderCreate()}
       </div>
     );
   }
@@ -54,7 +66,8 @@ class StreamList extends React.Component {
 const mapStateToProps = state => {
   return { 
     streams: Object.values(state.streams),
-    currentUserId: state.auth.userId
+    currentUserId: state.auth.userId,
+    isSignedIn: state.auth.isSignedIn
    };
 }
 
